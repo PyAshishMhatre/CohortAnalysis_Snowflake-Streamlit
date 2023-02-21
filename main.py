396e6a54a671a6007df8c281ff68341306c1f6b7
Ashish Mhatre <ashishmhatre927@gmail.com>
2023-02-21 09:43:40 -0500

    First commint

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import avg, sum, col,lit
+
+def create_session_object():
+   connection_parameters = {
+      "account": "ca60211.us-east4.gcp",
+      "user": "PYASHISHMHATRE",
+      "password": "!Ashish123",
+      "role": "SYSADMIN",
+      "warehouse": "COMPUTE_WH",
+      "database": "SPROCKET",
+      "schema": "PUBLIC"
+   }
+   session = Session.builder.configs(connection_parameters).create()
+   return session
+
+
+def load_data(session,table_name):
+    # CO2 Emissions by Country
+    # Complete database
+    df_trans = session.table(table_name)
+    df_trans = df_trans.to_pandas()
+    return df_trans
+
+
+session = create_session_object()
+data = load_data(session,'biketransactions')
+
+
+print(data.head())
+
+session.close()
